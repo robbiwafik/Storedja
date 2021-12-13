632b101a2fb4ea728d0f5c877fa786e438fdc6de
Clementine Rin <clementinerin09@gmail.com>
2021-12-13 09:05:26 +0700

    Register the store app models to admin site ...
    Registered Models:
        - Collection
        - Product
        - Customer
        - Order

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.db.models import query
+from django.urls import reverse
+from django.utils.html import format_html, urlencode
+from .models import Collection, Customer, Order, Product
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'unit_price',
+                    'inventory', 'last_update', 'collection']
+    list_per_page = 10
+
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'featured_product', 'products_count']
+    list_per_page = 10
+
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        query_str = urlencode({'collection__id': str(collection.id)})
+        url = reverse('admin:store_product_changelist') + '?' + query_str
+        return format_html('<a href="{}">{}</a>', url, collection.products.count())
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'payment_status', 'customer']
+    list_per_page = 10
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name',
+                    'email', 'phone', 'birth_date', 'membership']
+    list_per_page = 10
